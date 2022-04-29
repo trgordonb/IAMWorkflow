16c892a879aa3eca492b05fad8f2eff13867fd26
trgordonb <gordoncheng@outlook.com>
2022-04-29 19:57:44 +0800

    tmp disable auth route

--- index.js
+++ index.js
@@ -17,37 +17,30 @@ if (process.env.NODE_ENV !== 'production') {
 
 async function main() {
     const app = express()
-    try {
-        await mongoose.connect(MONGO_URL)
-    } 
-    catch (err) {
-        if (err.name === 'MongooseServerSelectionError') {
-            console.log(err.reason)
-        }
-    }
-    //await mongoose.connect(MONGO_URL, { useNewUrlParser: true })
+   
+    await mongoose.connect(MONGO_URL)
 
     const adminJs = new AdminJS(adminJsConfig.adminJsConfig)
-    //const router = AdminJSExpress.buildRouter(adminJs)
-    const router = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
-        authenticate: async (userId, password) => {
-            let user = await UserModel.findOne({ userId })
-            if (user) {
-                const matched = await bcrypt.compare(password, user.encryptedPassword)
-                if (matched) {
-                    return {
-                        email: user.userId,
-                        title: user.role,
-                        id: user._id,
-                        role: user.role,
-                        avatarUrl: `https://ui-avatars.com/api/?name=${user.userId}`
-                    }
-                }
-            }
-            return false
-        },
-        cookiePassword: process.env.COOKIE_PASSWORD
-    })
+    const router = AdminJSExpress.buildRouter(adminJs)
+    //const router = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+    //    authenticate: async (userId, password) => {
+    //        let user = await UserModel.findOne({ userId })
+    //        if (user) {
+    //            const matched = await bcrypt.compare(password, user.encryptedPassword)
+    //            if (matched) {
+    //                return {
+    //                    email: user.userId,
+    //                    title: user.role,
+    //                    id: user._id,
+    //                    role: user.role,
+    //                    avatarUrl: `https://ui-avatars.com/api/?name=${user.userId}`
+    //                }
+    //            }
+    //        }
+    //        return false
+    //    },
+    //    cookiePassword: process.env.COOKIE_PASSWORD
+    //})
     app.use(adminJs.options.rootPath, router)
     await app.listen(process.env.PORT, () => { console.log('AdminJS is at localhost:8080/admin') })
 }
