c7d0a6e78c7b595810d69695d2c52a6f9515cd98
trgordonb <gordoncheng@outlook.com>
2022-04-29 20:04:34 +0800

    turn back on auth route

--- index.js
+++ index.js
@@ -21,26 +21,25 @@ async function main() {
     await mongoose.connect(MONGO_URL)
 
     const adminJs = new AdminJS(adminJsConfig.adminJsConfig)
-    const router = AdminJSExpress.buildRouter(adminJs)
-    //const router = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
-    //    authenticate: async (userId, password) => {
-    //        let user = await UserModel.findOne({ userId })
-    //        if (user) {
-    //            const matched = await bcrypt.compare(password, user.encryptedPassword)
-    //            if (matched) {
-    //                return {
-    //                    email: user.userId,
-    //                    title: user.role,
-    //                    id: user._id,
-    //                    role: user.role,
-    //                    avatarUrl: `https://ui-avatars.com/api/?name=${user.userId}`
-    //                }
-    //            }
-    //        }
-    //        return false
-    //    },
-    //    cookiePassword: process.env.COOKIE_PASSWORD
-    //})
+    const router = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+        authenticate: async (userId, password) => {
+            let user = await UserModel.findOne({ userId })
+            if (user) {
+                const matched = await bcrypt.compare(password, user.encryptedPassword)
+                if (matched) {
+                    return {
+                        email: user.userId,
+                        title: user.role,
+                        id: user._id,
+                        role: user.role,
+                        avatarUrl: `https://ui-avatars.com/api/?name=${user.userId}`
+                    }
+                }
+            }
+            return false
+        },
+        cookiePassword: process.env.COOKIE_PASSWORD
+    })
     app.use(adminJs.options.rootPath, router)
     await app.listen(process.env.PORT, () => { console.log('AdminJS is at localhost:8080/admin') })
 }
