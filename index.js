fb88d95289873d3af8a91726436622e762963b5f
trgordonb <gordoncheng@outlook.com>
2022-04-29 20:14:05 +0800

    change mongo uri format

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const UserModel = require('./models/user.model')
 AdminJS.registerAdapter(AdminJsMongoose)
 let MONGO_URL = ''
 if (process.env.NODE_ENV !== 'production') {
-    MONGO_URL = process.env.MONGO_URL
+    MONGO_URL = `mongodb://root:${process.env.MONGO_URL}:27017/IAMTest?authSource=admin`
 } else {
     MONGO_URL = `mongodb://root:${process.env.MONGO_PASSWORD}@${process.env.REPLICASET_1}:27017,${process.env.REPLICASET_2}:27017/test?authSource=admin&replicaSet=rs0`
 }
